581d7a2aaf5f391c9af52375db0a227e1c350e3b
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-10-28 13:37:44 +0800

    Set up database test

--- tests/server/testIndex.test.js
+++ tests/server/testIndex.test.js
@@ -2,7 +2,7 @@ import testIndex from "../../src/server/controllers/testController.js";
 import request from "supertest";
 import express from "express";
 import { test } from "vitest";
-import dotenv from dotenv;
+import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
@@ -12,16 +12,20 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use("/", testIndex);
 
-const databseURL = process.env.NODE_ENV === "test" ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL
+const databaseURL =
+  process.env.NODE_ENV === "test" ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL;
 
 const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: databseURL
+      url: databaseURL
     }
   }
-})
+});
 
-test("index route works", (done) => {
-  request(app).get("/").expect("Content-Type", /json/).expect({ name: "frodo" }).expect(200, done);
+test("index route works", async () => {
+  const response = await request(app).get("/");
+  // expect(response.headers["Content-Type"]).toMatch(/json/);
+  expect(response.status).toEqual(200);
+  expect(response.body.name).toEqual("frodo");
 });
