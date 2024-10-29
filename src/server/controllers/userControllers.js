1915334c0d7de22b712a9c0d1d9d26fab6fd5121
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-10-29 14:49:03 +0800

    Started signup, login, and logout controllers.

---
+++ src/server/controllers/userControllers.js
@@ -0,0 +1,73 @@
+import { PrismaClient } from "@prisma/client";
+import expressAsyncHandler from "express-async-handler";
+import dotenv from "dotenv";
+import { body, validationResult } from "express-validator";
+import bcrypt from "bcryptjs";
+import passport from "passport";
+
+const prisma = new PrismaClient();
+
+dotenv.config();
+
+const signup = [
+  // Validate inputs here
+  //
+  expressAsyncHandler(async (req, res, next) => {
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+      if (err) {
+        return next(err);
+      } else {
+        const errors = validationResult(req);
+        const usernameTaken = await prisma.user.findFirst({
+          where: req.body.username
+        });
+        if (!errors.isEmpty() || usernameTaken) {
+          res.status(200).json(false);
+          return;
+        }
+        await prisma.user.create({
+          data: {
+            username: req.body.username,
+            password: hashedPassword
+          }
+        });
+        res.status(200).json(user);
+      }
+    });
+  })
+];
+
+export const login = [
+  expressAsyncHandler(async (req, res, next) => {
+    const user = await prisma.user.findFirst({
+      where: {
+        username: req.body.username
+      }
+    });
+    if (!user) {
+      res.status(200).json(false);
+      return;
+    }
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (!match) {
+      res.status(200).json(false);
+      return;
+    }
+    next();
+  }),
+  passport.authenticate("local"),
+  (req, res, next) => {
+    res.status(200).json(req.user);
+  }
+];
+
+export const logout = (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+};
+
+export default signup;
