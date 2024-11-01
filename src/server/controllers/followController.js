9367ed30b0edc8f60235bd9be7481da9f9c3a38b
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-11-01 14:21:13 +0800

    Created post_follow_user. Checks if users already follow eachother, checks if a request is sent, deletes sent request, then creates a follow link between users.

--- src/server/controllers/followController.js
+++ src/server/controllers/followController.js
@@ -4,6 +4,84 @@ import { body, validationResult } from "express-validator";
 
 const prisma = new PrismaClient();
 
-const post_follow_user = expressAsyncHandler(async (req, res, next) => {});
+const post_follow_user = expressAsyncHandler(async (req, res, next) => {
+  const alreadyFollowing = await prisma.follow.findFirst({
+    where: {
+      OR: [
+        {
+          followedById: req.body.sender,
+          followingId: req.body.receiver
+        },
+        {
+          followedById: req.body.receiver,
+          followingId: req.body.sender
+        }
+      ]
+    }
+  });
+  const request = await prisma.request.findFirst({
+    where: {
+      OR: [
+        {
+          senderId: req.body.sender,
+          receiverId: req.body.receiver
+        },
+        {
+          senderId: req.body.receiver,
+          receiverId: req.body.sender
+        }
+      ]
+    }
+  });
+  if (alreadyFollowing || !request) {
+    res.status(200).json(false);
+    return;
+  }
+  await prisma.request.delete({
+    where: {
+      id: request.id
+    }
+  });
+  const followUser = await prisma.follow.create({
+    data: {
+      followedById: req.body.sender,
+      followingId: req.body.receiver
+    }
+  });
+  const requestList = await prisma.request.findMany({
+    where: {
+      OR: [
+        {
+          senderId: req.body.sender
+        },
+        {
+          receiverId: req.body.sender
+        }
+      ]
+    },
+    include: {
+      sender: true,
+      receiver: true
+    }
+  });
+  const followList = await prisma.follow.findMany({
+    where: {
+      OR: [
+        {
+          followedById: req.body.sender
+        },
+        {
+          followingId: req.body.sender
+        }
+      ]
+    },
+    include: {
+      followedBy: true,
+      following: true
+    }
+  });
+  // Send follows and requests instead?
+  res.status(200).json({ requestList: requestList, followList: followList });
+});
 
 export default post_follow_user;
