1c55577e8564950f9e07d93b53c4bbd0bc79c19c
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-11-01 11:10:37 +0800

    Deleted

--- src/server/controllers/testController.js
+++
@@ -1,7 +0,0 @@
-import expressAsyncHandler from "express-async-handler";
-
-const testIndex = expressAsyncHandler(async (req, res, next) => {
-  res.json({ name: "frodo" });
-});
-
-export default testIndex;
