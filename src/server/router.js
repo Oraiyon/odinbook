1aa68a32620975cdd1454f344f399e1ff94a7470
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-11-01 14:18:10 +0800

    Deleted get_requests route

--- src/server/router.js
+++ src/server/router.js
@@ -1,7 +1,7 @@
 import express from "express";
 import signup, { login, logout } from "./controllers/userControllers.js";
 import post_follow_user from "./controllers/followController.js";
-import post_request_follow, { get_requests } from "./controllers/requestController.js";
+import post_request_follow from "./controllers/requestController.js";
 
 const router = express.Router();
 
@@ -12,9 +12,8 @@ router.get("/logout", logout);
 
 // requestController
 router.post("/api/requestFollow", post_request_follow);
-router.get("/api/getRequests/:id", get_requests);
 
 // followController
-// router.post("/api/followuser", post_follow_user);
+router.post("/api/followuser", post_follow_user);
 
 export default router;
