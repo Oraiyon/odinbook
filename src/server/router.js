f65c0e3ffee418a8f18afb94497af2242a92f913
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-11-02 09:23:12 +0800

    Updated filenames

--- src/server/router.js
+++ src/server/router.js
@@ -1,21 +1,23 @@
 import express from "express";
 import signup, { login, logout } from "./controllers/userControllers.js";
-import post_request, { delete_request } from "./controllers/requestController.js";
-import post_follow, { delete_follow } from "./controllers/followController.js";
+import post_request, { delete_request } from "./controllers/requestControllers.js";
+import post_follow, { delete_follow } from "./controllers/followControllers.js";
 
 const router = express.Router();
 
-// userController
+// userControllers
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/logout", logout);
 
-// requestController
+// requestControllers
 router.post("/api/requestFollow", post_request);
 router.delete("/api/deleteRequest", delete_request);
 
-// followController
+// followControllers
 router.post("/api/followuser", post_follow);
 router.delete("/api/deleteFollow", delete_follow);
+
+// postControllers
 
 export default router;
