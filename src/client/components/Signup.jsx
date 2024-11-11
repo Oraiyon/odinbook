e99628bf51e0b2f350e2fce497a8230f69b53cab
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-11-11 15:27:53 +0800

    Started page

--- src/client/components/Signup.jsx
+++ src/client/components/Signup.jsx
@@ -5,25 +5,52 @@ const Signup = () => {
   const usernameInputRef = useRef(null);
   const passwordInputRef = useRef(null);
   const confirmPasswordInputRef = useRef(null);
+  const invalidUsernameRef = useRef(null);
+  const invalidPasswordRef = useRef(null);
+  const invalidConfirmPasswordRef = useRef(null);
 
   const submitSignup = async (e) => {
     try {
       e.preventDefault();
-      // Validate here
-      await fetch("/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          username: usernameInputRef.current.value,
-          password: passwordInputRef.current.value,
-          confirmPassword: confirmPasswordInputRef.current.value
-        })
-      });
+      const validInputs = validateSignupInputs();
+      if (validInputs) {
+        await fetch("/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            username: usernameInputRef.current.value,
+            password: passwordInputRef.current.value,
+            confirmPassword: confirmPasswordInputRef.current.value
+          })
+        });
+      }
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const validateSignupInputs = () => {
+    let validInputs = true;
+    if (
+      !usernameInputRef.current.value ||
+      usernameInputRef.current.value.length < 3 ||
+      usernameInputRef.current.value > 10
+    ) {
+      invalidUsernameRef.current.value =
+        "Usernames must be atleast 3 characters long and atmost 10 characters long.";
+      validInputs = false;
+    }
+    if (!passwordInputRef.current.value || passwordInputRef.current.value.length < 3) {
+      invalidPasswordRef.current.value = "Passwords must be atleast 6 characters long.";
+      validInputs = false;
+    }
+    if (passwordInputRef.current.value !== confirmPasswordInputRef.current.value) {
+      invalidConfirmPasswordRef.current.value = "Confirm Passwords must match your Password.";
+      validInputs = false;
+    }
+    return validInputs;
   };
 
   return (
@@ -33,10 +60,12 @@ const Signup = () => {
         <div className={styles.username_input}>
           <label htmlFor="username">Username</label>
           <input type="text" id="username" name="username" ref={usernameInputRef} />
+          <p ref={invalidUsernameRef}></p>
         </div>
         <div className={styles.password_input}>
           <label htmlFor="password">Password</label>
           <input type="password" id="password" name="password" ref={passwordInputRef} />
+          <p ref={invalidPasswordRef}></p>
         </div>
         <div className={styles.confirm_password_input}>
           <label htmlFor="confirmPassword">Confirm Password</label>
@@ -46,6 +75,7 @@ const Signup = () => {
             name="confirmPassword"
             ref={confirmPasswordInputRef}
           />
+          <p ref={invalidConfirmPasswordRef}></p>
         </div>
         <button>Sign up</button>
       </form>
