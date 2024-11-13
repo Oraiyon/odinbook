fceaa6974fa9eb12958a5b210d2611e31cd7dad0
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-11-13 11:50:26 +0800

    Added useEffect for getting posts to PostList.jsx

--- src/client/components/PostList.jsx
+++ src/client/components/PostList.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import styles from "../stylesheets/PostList.module.css";
 
 const PostList = (props) => {
+  // props.user for following or separate component?
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const response = await fetch("/api/get/posts");
+        const data = await response.json();
+        setPostList(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getPosts();
+  }, []);
+
   return (
     <div className={styles.post_container}>
-      {props.postList
-        ? props.postList.map((post) => (
+      {postList
+        ? postList.map((post) => (
             <div key={post.id} className={styles.post_card}>
               <p>{post.author.username}</p>
               <p>{post.text}</p>
