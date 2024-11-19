31c8f7eb77c472180009d40e9adf74b766d11230
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-11-19 11:18:53 +0800

    Updated styling

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
+import styles from "../stylesheets/App.module.css";
 
 function App() {
   const [user, setUser] = useState(null);
 
   return (
-    <>
+    <div className={styles.app_container}>
       <Outlet context={[user, setUser]} />
       <Navbar user={user} />
-    </>
+    </div>
   );
 }
 
