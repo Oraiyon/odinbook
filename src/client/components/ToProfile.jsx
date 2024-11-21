70c0a75fb6a959d256aa71d6d75da8a470676e45
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-11-21 12:06:48 +0800

    Removed props.mode

--- src/client/components/ToProfile.jsx
+++ src/client/components/ToProfile.jsx
@@ -1,23 +1,12 @@
 import { Link } from "react-router-dom";
 import styles from "../stylesheets/ToProfile.module.css";
-import BackHeader from "./BackHeader";
 
 const ToProfile = (props) => {
-  if (props.mode === "profile") {
-    //
-    return (
-      <div className={styles.user_card}>
-        {/* <Link to={`/${props.user.id}/profile`}>{props.user.username}</Link> */}
-        <BackHeader mode={"profile"} user={props.user.username} />
-      </div>
-    );
-  } else {
-    return (
-      <div className={styles.user_card}>
-        <Link to={`/${props.user.id}/profile`}>{props.user.username}</Link>
-      </div>
-    );
-  }
+  return (
+    <div className={styles.user_card}>
+      <Link to={`/${props.user.id}/profile`}>{props.user.username}</Link>
+    </div>
+  );
 };
 
 export default ToProfile;
