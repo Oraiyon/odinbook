cde9c9aed46019950ddee27a3b109912bc3265a5
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-04-02 08:59:05 +0800

    added session

--- src/server/main.js
+++ src/server/main.js
@@ -1,7 +1,7 @@
 import express from "express";
 import ViteExpress from "vite-express";
 import passport from "passport";
-// import session from "express-session";
+import session from "express-session";
 import dotenv from "dotenv";
 import router from "./router.js";
 import initializePassport from "./passport_config.js";
@@ -19,21 +19,21 @@ dotenv.config();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(
-//   session({
-//     secret: process.env.SECRET,
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: {
-//       maxAge: 1000 * 60 * 60 * 24
-//     }
-//     // store: new PrismaSessionStore(new PrismaClient(), {
-//     //   checkPeriod: 2 * 60 * 1000,
-//     //   dbRecordIdIsSessionId: true,
-//     //   dbRecordIdFunction: undefined
-//     // })
-//   })
-// );
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24
+    }
+    // store: new PrismaSessionStore(new PrismaClient(), {
+    //   checkPeriod: 2 * 60 * 1000,
+    //   dbRecordIdIsSessionId: true,
+    //   dbRecordIdFunction: undefined
+    // })
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 initializePassport(passport);
