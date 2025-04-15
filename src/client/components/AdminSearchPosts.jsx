1c159b255fdde82ccf9bbcaf998070c8f192670f
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-04-15 14:18:12 +0800

    Updated

--- src/client/components/AdminSearchPosts.jsx
+++ src/client/components/AdminSearchPosts.jsx
@@ -3,7 +3,8 @@ import styles from "../stylesheets/AdminSearchPosts.module.css";
 
 const AdminSearchPost = (props) => {
   const [postList, setPostList] = useState([]);
-  const [deletedPostIds, setDeletedPostIds] = useState([]);
+  const [deletedPosts, setDeletedPosts] = useState([]);
+  const [deletePostsId, setDeletedPostsId] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -31,29 +32,35 @@ const AdminSearchPost = (props) => {
     }
   }, [props.searchedText]);
 
-  const handleSelectPost = (id) => {
-    for (let i = 0; i < deletedPostIds.length; i++) {
-      if (deletedPostIds[i] === id) {
-        const newArray = deletedPostIds.filter((item) => item !== id);
-        setDeletedPostIds(newArray);
+  const handleSelectPost = (post) => {
+    for (let i = 0; i < deletedPosts.length; i++) {
+      if (deletedPosts[i].id === post.id) {
+        const newArray = deletedPosts.filter((item) => item.id !== post.id);
+        const newArrayId = deletePostsId.filter((item) => item !== post.id);
+        setDeletedPosts(newArray);
+        setDeletedPostsId(newArrayId);
         return;
       }
     }
-    setDeletedPostIds((d) => [...d, id]);
+    setDeletedPosts((d) => [...d, post]);
+    setDeletedPostsId((d) => [...d, post.id]);
   };
 
   const handlePostDelete = async () => {
     try {
-      if (deletedPostIds.length) {
+      if (deletedPosts.length) {
         let data;
-        for (let i = 0; i < deletedPostIds.length; i++) {
-          const response = await fetch(`/api/${selectedUser.id}/delete/${deletedPostIds[i]}`, {
-            method: "DELETE"
-          });
+        for (let i = 0; i < deletedPosts.length; i++) {
+          const response = await fetch(
+            `/api/${deletedPosts[i].authorId}/delete/${deletedPosts[i].id}`,
+            {
+              method: "DELETE"
+            }
+          );
           data = await response.json();
         }
-        setSelectedUserPostList(data);
-        setDeletedPostIds([]);
+        setPostList(data);
+        setDeletedPosts([]);
       }
     } catch (error) {
       console.log(error);
@@ -71,14 +78,14 @@ const AdminSearchPost = (props) => {
           onChange={(e) => props.setSearchedText(e.target.value)}
         />
       </form>
-      <p>Posts To Be Deleted: {deletedPostIds.length}</p>
+      <p>Posts To Be Deleted: {deletedPosts.length}</p>
       <button onClick={handlePostDelete}>Delete</button>
       <div className={styles.postList}>
         {postList.map((post) => (
           <div
             key={post.id}
-            className={deletedPostIds.includes(post.id) ? styles.selected_post_card : ""}
-            onClick={() => handleSelectPost(post.id)}
+            className={deletePostsId.includes(post.id) ? styles.selected_post_card : ""}
+            onClick={() => handleSelectPost(post)}
           >
             <p>{post.author.username}</p>
             <img src={post.image} alt="" />
