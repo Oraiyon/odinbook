6e13f5834048114f598b76b46d1955a9ef9b9200
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-05-12 13:52:43 +0800

    Updated conditional

--- src/client/components/Navbar.jsx
+++ src/client/components/Navbar.jsx
@@ -2,17 +2,9 @@ import { Link } from "react-router-dom";
 import styles from "../stylesheets/Navbar.module.css";
 
 const Navbar = (props) => {
-  if (!props.user) {
-    return (
-      <nav className={styles.navbar_container}>
-        <Link to={"/signup"}>Sign Up</Link>
-        <Link to={"/"}>Search</Link>
-        <Link to={"/login"}>Log In</Link>
-      </nav>
-    );
-  } else if (props.user && props.user.admin) {
+  if (props.user && props.user.admin) {
     return "";
-  } else {
+  } else if (props.user && !props.user.admin) {
     return (
       <nav className={styles.navbar_container}>
         <Link to={"/feed"}>Feed</Link>
@@ -20,6 +12,14 @@ const Navbar = (props) => {
         <Link to={"/post"}>Post</Link>
         <Link to={"/inbox"}>Inbox</Link>
         <Link to={"/user"}>User</Link>
+      </nav>
+    );
+  } else {
+    return (
+      <nav className={styles.navbar_container}>
+        <Link to={"/signup"}>Sign Up</Link>
+        <Link to={"/"}>Search</Link>
+        <Link to={"/login"}>Log In</Link>
       </nav>
     );
   }
