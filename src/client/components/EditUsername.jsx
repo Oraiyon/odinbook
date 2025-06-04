44316aedc942187479a81cb9ec9fcddcb58a9bc0
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-06-04 12:31:02 +0800

    Styled EditUsername component

--- src/client/components/EditUsername.jsx
+++ src/client/components/EditUsername.jsx
@@ -8,22 +8,25 @@ const EditUsername = () => {
 
   const usernameRef = useRef(null);
 
-  const editUsername = async () => {
+  const editUsername = async (e) => {
     try {
-      const response = await fetch("/api/user/edit/username", {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          id: user.id,
-          username: usernameRef.current.value
-        })
-      });
-      const data = await response.json();
-      if (data) {
-        setUser(data);
-        usernameRef.current.value = "";
+      e.preventDefault();
+      if (usernameRef.current.value.length >= 3 && usernameRef.current.value.length <= 20) {
+        const response = await fetch("/api/user/edit/username", {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            id: user.id,
+            username: usernameRef.current.value
+          })
+        });
+        const data = await response.json();
+        if (data) {
+          setUser(data);
+          usernameRef.current.value = "";
+        }
       }
     } catch (error) {
       console.log(error);
@@ -33,12 +36,12 @@ const EditUsername = () => {
   return (
     <div className={styles.editUsername_container}>
       <BackHeader mode={null} user={user} />
-      <form onSubmit={editUsername}>
+      <form>
         <div>
           <label htmlFor="edit_username">Edit Username</label>
           <input type="text" id="edit_username" placeholder={user.username} ref={usernameRef} />
         </div>
-        <button>Submit Username</button>
+        <button onClick={(e) => editUsername(e)}>Submit Username</button>
       </form>
     </div>
   );
